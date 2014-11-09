a9fdd68627106e731524df254d83170bad4f9860
Christopher Rosengren <christopher.rosengren@gmail.com>
2014-11-09 16:01:11 +1100

    Deps

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject com.eoneq/time "0.2.2"
-  :dependencies [[clj-time "0.7.0"]
-                 [org.clojure/clojure "1.6.0"]])
+(defproject com.eoneq/time "0.2.3"
+  :dependencies [[clj-time "0.8.0"]
+                 [org.clojure/clojure "1.6.0"]]
+  :global-vars {*warn-on-reflection* true})
