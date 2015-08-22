e6c4c989068bcb87eee4749e50bf3ca39c7461e8
Christopher Rosengren <christopher.rosengren@gmail.com>
2015-08-22 15:57:07 +1000

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.eoneq/time "0.2.7"
-  :dependencies [[clj-time "0.10.0"]
+(defproject com.eoneq/time "0.2.8"
+  :dependencies [[clj-time "0.11.0"]
                  [org.clojure/clojure "1.7.0"]]
   :global-vars {*warn-on-reflection* true})
