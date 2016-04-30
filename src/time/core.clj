11646635d069da2ef5279283804b99e5834a7b1b
Christopher Rosengren <christopher.rosengren@gmail.com>
2016-04-30 13:45:10 +1000

    Whitespace

--- src/time/core.clj
+++ src/time/core.clj
@@ -5,23 +5,38 @@
 (defn n-days-ago
   "dt, n days ago from local date time"
   [n]
-  (let [dt (co/to-date-time (t/today))]
-    (t/minus dt (t/days n))))
+  (let [dt
+        (co/to-date-time (t/today))]
+    (t/minus dt
+             (t/days n))))
 
 (defn dates
   "dt's from-to, inclusive"
   [from to]
-  (take-while (fn [dt] (or (t/before? dt to) (= dt to)))
-              (iterate #(t/plus % (t/days 1)) from)))
+  (take-while (fn [dt]
+                (or (t/before? dt to)
+                    (= dt to)))
+              (iterate #(t/plus % (t/days 1))
+                       from)))
 
 (defn n-last-days
   "An ordered sequence of the n last days in dts from today, inclusive"
-  ([n] (n-last-days (co/to-date-time (t/today)) n))
-  ([dt n] (reverse (dates (t/minus dt (t/days (dec n))) dt))))
+  ([n]
+   (n-last-days (co/to-date-time (t/today))
+                n))
+  ([dt n]
+   (reverse (dates (t/minus dt
+                            (t/days (dec n)))
+                   dt))))
 
 (defn- pprint
   [dt s]
-  (format "%04d%s%02d%s%02d" (t/year dt) s (t/month dt) s (t/day dt)))
+  (format "%04d%s%02d%s%02d"
+          (t/year dt)
+          s
+          (t/month dt)
+          s
+          (t/day dt)))
 
 (defn concise
   [dt]
@@ -39,8 +54,13 @@
 
 (defn current-date-map
   [str-time-zone]
-  (let [time-zone (t/time-zone-for-id str-time-zone)
-        dt (t/to-time-zone (t/now) time-zone)]
+  (let [time-zone
+        (t/time-zone-for-id str-time-zone)
+        
+        dt
+        (t/to-time-zone (t/now)
+                        time-zone)]
+    
     (dt->date-map dt)))
 
 (defn within?
