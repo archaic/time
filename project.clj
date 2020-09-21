439d0779e80b25b1ae6a52c92c6be59c59b5d0a0
Christopher Rosengren <christopher.rosengren@eoneq.com>
2020-09-21 12:46:27 +1000

    update

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject com.eoneq/time "0.2.10"
-
-  :global-vars {*warn-on-reflection* true}
-  
-  :lein-tools-deps/config {:config-files [:install :user :project]}
-
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  
-  :plugins [[lein-tools-deps "0.4.5"]])
